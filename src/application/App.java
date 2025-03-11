647c08953611d802aaf52a1a63c4fe2dee8680f9
Rafael Lima <rafaellima61app@gmail.com>
2025-03-11 14:04:53 -0300

    Desafio de conta bancaria concluído

--- src/application/App.java
+++ src/application/App.java
@@ -1,4 +1,39 @@
 package application;
 
+import dominio.Account;
+
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Account account = new Account();
+
+        System.out.println("Enter account number: ");
+        account.setNumber(sc.nextInt());
+        sc.nextLine();
+
+        System.out.println("Type your name: ");
+        account.setName(sc.nextLine());
+
+        System.out.println("Enter initial deposit value: ");
+        account.setDepositValue(sc.nextDouble());
+
+        System.out.println("Account data: ");
+        System.out.printf("Account %d, Holder: %s, Balance: $%.2f%n", account.getNumber(), account.getName(), account.getDepositValue());
+
+        System.out.println("Enter a deposit value: ");
+        double deposit = sc.nextDouble();
+        account.addDeposit(deposit);
+
+        System.out.printf("Updated account %d, Holder: %s, Balance: $%.2f%n", account.getNumber(), account.getName(), account.getDepositValue());
+
+        System.out.println("Enter withdraw value: ");
+        double withdraw = sc.nextDouble();
+        account.withDrawDeposit(withdraw);
+
+        System.out.printf("Updated account %d, Holder: %s, Balance: $%.2f%n", account.getNumber(), account.getName(), account.getDepositValue());
+
+        sc.close();
+    }
 }
--- src/dominio/Account.java
+++ src/dominio/Account.java
@@ -1,4 +1,49 @@
 package dominio;
 
 public class Account {
+    private String name;
+    private int number;
+    private double depositValue;
+
+    public Account(String name, int number, boolean initialDeposit, double depositValue) {
+        this.name = name;
+        this.number = number;
+        this.depositValue = depositValue;
+    }
+
+    public Account() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getDepositValue() {
+        return depositValue;
+    }
+
+    public void setDepositValue(double depositValue) {
+        this.depositValue = depositValue;
+      }
+    public void addDeposit(double amount) {
+        this.depositValue += amount;
+    }
+
+    public void withDrawDeposit(double amount) {
+        double fee = 5.00;
+        this.depositValue -= (amount + fee);
+    }
 }
